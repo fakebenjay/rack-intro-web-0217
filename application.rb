49c7e1f4e9f90bc2f4ad4e5fc940b1f8d528767c
fakebenjay <fakebenjay@gmail.com[color]>
2017-02-19 19:00:16 -0500

    Done.

--- application.rb
+++ application.rb
@@ -2,9 +2,8 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, I am referred to by my friends and family by my name, which is 'Birdperson.'"
     resp.finish
   end
 
 end
-
--- spec/rack_mechanics_spec.rb
+++ spec/rack_mechanics_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper.rb'
 describe "Application" do
   it "should introduce itself" do
     get "/"
-    expect(last_response.body).to include ("Hello, my name is")
+    expect(last_response.body).to include ("Hello, I am referred to by my friends and family by my name, which is")
   end
 end
